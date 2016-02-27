6a1c2c4b1bc84c59c8b57be71549fe57acef67fe
Gordan Kreković <gordank@gmail.com>
2016-02-27 18:57:28 +0100

    Double quotes replaced with apostrophes to work with Matlab

--- parsetime.m
+++ parsetime.m
@@ -2,7 +2,7 @@
 ## Created: 2015-08-07
 
 function [retval] = parsetime (tstemp)
-  s = strsplit(tstemp, ".");
-  tm_struct = strptime(s{1}, "%Y-%m-%d %T");
+  s = strsplit(tstemp, '.');
+  tm_struct = strptime(s{1}, '%Y-%m-%d %T');
   retval = mktime(tm_struct);
 end
