b7f2aab05f3f8238ab1686d5648d0a608ed3e5a3
Gordan Kreković <gordank@gmail.com>
2016-02-27 18:58:33 +0100

    Double quotes replaced with apostrophes to work with Matlab

--- main.m
+++ main.m
@@ -17,12 +17,12 @@ ch = 2;
 % Duration of the time period of the data set in seconds
 dataduration = 86400;
 
-piece = makecloud("AFS_raw_clicks.csv", duration, fs, ch, dataduration, 0);
+piece = makecloud('AFS_raw_clicks.csv', duration, fs, ch, dataduration, 0);
 % piece = makecloud("AFS_raw_conversions.csv", duration, fs, ch, dataduration, 1);
 
 maxsample = max(max(piece(:, :)));
 
 % Export each channel to a separate wave file
 for i = 1:ch
-  wavwrite(piece(:, i) / maxsample, fs, 16, ["clks" int2str(i) ".wav"]);
+  wavwrite(piece(:, i) / maxsample, fs, 16, ['clks' int2str(i) '.wav']);
 end
