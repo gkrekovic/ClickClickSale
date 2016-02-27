4026f28efa8c7112bf04abf2c623f3f141076f42
Gordan Kreković <gordank@gmail.com>
2016-02-27 19:00:10 +0100

    Comment edited

--- makewindow.m
+++ makewindow.m
@@ -1,25 +1,4 @@
-## Copyright (C) 2015 gkrekovic
-## 
-## This program is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-## -*- texinfo -*- 
-## @deftypefn {Function File} {@var{retval} =} makewindow (@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
-
-## Author: gkrekovic <gkrekovic@NTH-HR-ZG-0463>
+## Author: Gordan Kreković
 ## Created: 2015-08-07
 
 function [retval] = makewindow (conn, len, typ)
