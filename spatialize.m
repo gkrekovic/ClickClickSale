c76e277112e94f1a22f6c3a299c5cbd960f1d251
Gordan Kreković <gordank@gmail.com>
2016-02-27 19:01:52 +0100

    Update spatialize.m

--- spatialize.m
+++ spatialize.m
@@ -6,25 +6,25 @@ function [retval] = spatialize (country, ch)
   rloc = 0;
   philoc = 0;
   
-  if strcmp(strtrim(country), "UNITED STATES")
+  if strcmp(strtrim(country), 'UNITED STATES')
     rloc = normrnd(3*pi/4, pi/4);
     philoc = min([1 normrnd(0.7, 0.2)]);
-  elseif strcmp(strtrim(country), "CANADA")
+  elseif strcmp(strtrim(country), 'CANADA')
     rloc = normrnd(pi/2, pi/10);
     philoc = min([1 normrnd(0.9, 0.1)]);
-  elseif strcmp(strtrim(country), "AUSTRALIA")
+  elseif strcmp(strtrim(country), 'AUSTRALIA')
     rloc = normrnd(15*pi/8, pi/7);
     philoc = min([1 normrnd(0.6, 0.3)]);
-  elseif strcmp(strtrim(country), "NEW ZEALAND")
+  elseif strcmp(strtrim(country), 'NEW ZEALAND')
     rloc = normrnd(7*pi/8, pi/9);
     philoc = min([1 normrnd(0.9, 0.1)]);
-  elseif strcmp(strtrim(country), "CHINA")
+  elseif strcmp(strtrim(country), 'CHINA')
     rloc = normrnd(0, pi/6);
     philoc = min([1 normrnd(0.8, 0.3)]);
-  elseif strcmp(strtrim(country), "JAPAN")
+  elseif strcmp(strtrim(country), 'JAPAN')
     rloc = normrnd(pi/9, pi/10);
     philoc = min([1 normrnd(0.9, 0.1)]);
-  elseif strcmp(strtrim(country), "UNITED KINGDOM")
+  elseif strcmp(strtrim(country), 'UNITED KINGDOM')
     rloc = normrnd(3*pi/8, pi/10);
     philoc = min([1 normrnd(0.6, 0.3)]);
   else
