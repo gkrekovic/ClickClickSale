eb0e362293eec35a700dfbd8db88289014a028c1
Gordan Krekovic <gordankrekovic@Gordans-MacBook-Pro.local>
2017-02-07 16:58:36 +0100

    Time parsing corrected.

--- parsetime.m
+++ parsetime.m
@@ -2,6 +2,6 @@
 % Created: 2015-08-07
 
 function [retval] = parsetime (tstemp)
-  t = datetime(tstemp, 'Format', 'yyyy-MM-dd HH:mm:SS.000');
+  t = datetime(tstemp, 'Format', 'yyyy-MM-dd HH:mm:ss.000');
   retval = posixtime(t);
 end
